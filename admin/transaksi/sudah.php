e5c66de4613ffd74befd325cb842a855ec3a22ba
Raihan Cahtra Rizqy Prian Pratama <58718267+Raihan-Creator@users.noreply.github.com>
2022-06-19 07:09:26 +0700

    Tambah sudah.php
    Menambahkan Link No Reload pada Sudah Bayar

---
+++ admin/transaksi/sudah.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Perhutani</title>
+    <style>
+        .card{
+        border-radius: 17px 17px 17px 17px;
+        box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+        }
+    </style>
+</head>
+<body>
+    <div class="nav-link">
+        <div class="card">
+            <div class="card-body">
+                <center>
+                    <img src="../dist/img/online-shopping.png" alt="cart" width="100px" height="100px">
+                    <h6>Belum ada pesanan</h6>
+                </center>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
