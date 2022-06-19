4ea6390c3fc9124de3adb738e979521a0eb79bb5
Raihan Cahtra Rizqy Prian Pratama <58718267+Raihan-Creator@users.noreply.github.com>
2022-06-19 07:13:36 +0700

    Menambahkan Fitur belum.php
    Menambahkan Fitur Link No Reload pada Belum Dibayar

---
+++ admin/transaksi/belum.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Perhutani</title>
+    <style>
+        .card{
+        border-radius: 17px 17px 17px 17px;
+        box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+        display: flex;
+        }
+    </style>
+</head>
+<body>
+    <div class="nav-link">
+        <div class="card">
+            <div class="card-body">
+                <h5 align="right"> Total Belum Dibayar </h5><hr>
+                <h5 align="right" class="mx-auto"> 1 Produk </h5>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <p align="left">
+                  <img src="../dist/img/kaos.jpg" alt="" width="150px" height="150px">
+                  <span> Kaos Hitam T-Shirt Polos House of Cuff Motif Tokyo Black </span>
+                </p>
+                <hr>
+                <p>
+                    <span> Kategori : <b>T-Shirt</b> </span><br>
+                    <span> Total Produk : <b>5</b> </span>
+                    <h4><b>Rp 85.000</b></h4>
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
