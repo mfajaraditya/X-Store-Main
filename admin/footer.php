44c2eeff4508a257c19d97d98a8d60b515c87bd3
Raihan Cahtra Rizqy Prian Pratama <58718267+Raihan-Creator@users.noreply.github.com>
2022-06-23 19:49:25 +0700

    Tambah footer.php
    untuk link no reload

---
+++ admin/footer.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard X-STORE</title>
+    <style>
+        .card{
+        border-radius: 17px 17px 17px 17px;
+        box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+        }
+    </style>
+</head>
+<body>
+<div data-bs-spy="scroll" data-bs-target="#list-example" data-bs-smooth-scroll="true" class="scrollspy-example float-center" tabindex="0">
+  <div class="card">
+  <div class="card-body" id="list-item-1">
+   <b> Footer </b><br><br>
+<form action="" method="post">
+<div class="row g-2">
+
+<div class="col-md">
+  <div class="form-floating">
+    <input type="email" class="form-control" id="floatingInputGrid" placeholder="name@example.com">
+    <label for="floatingInputGrid">Facebook Links</label>
+  </div>
+</div>
+
+<div class="col-md">
+  <div class="form-floating">
+  <input type="email" class="form-control" id="floatingInputGrid" placeholder="name@example.com">
+    <label for="floatingInputGrid">Instagram Links</label>
+  </div>
+</div>
+
+</div>
+
+<br>
+
+<div class="row g-2">
+
+<div class="col-md">
+  <div class="form-floating">
+    <input type="email" class="form-control" id="floatingInputGrid" placeholder="name@example.com">
+    <label for="floatingInputGrid">WhatsApp Links</label>
+  </div>
+</div>
+
+<div class="col-md">
+  <div class="form-floating">
+  <input type="email" class="form-control" id="floatingInputGrid" placeholder="name@example.com">
+    <label for="floatingInputGrid">Twitter Links</label>
+  </div>
+</div>
+
+</div>
+
+<br>
+
+<div class="row g-2">
+
+<div class="col-md">
+  <div class="form-floating">
+    <input type="email" class="form-control" id="floatingInputGrid" placeholder="name@example.com">
+    <label for="floatingInputGrid">YouTube Links</label>
+  </div>
+</div>
+
+<div class="col-md">
+  <div class="form-floating">
+  <input type="email" class="form-control" id="floatingInputGrid" placeholder="name@example.com">
+    <label for="floatingInputGrid">Linkedln Links</label>
+  </div>
+</div>
+
+</div><br>
+
+<button type="submit" class="btn btn-success"> Simpan </button>
+</form>
+  </div>
+</div>
+</div>
+</body>
+</html>
