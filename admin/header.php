1cd2463c7cd476b4ca9dfcc8b78ac4995ead6cef
Raihan Cahtra Rizqy Prian Pratama <58718267+Raihan-Creator@users.noreply.github.com>
2022-06-23 19:48:54 +0700

    Tambah Fitur header.php
    untuk link no reload

---
+++ admin/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard X-STORE</title>
+    <style>
+        .card{
+        border-radius: 17px 17px 17px 17px;
+        box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+        }
+    </style>
+</head>
+<body>
+<div data-bs-spy="scroll" data-bs-target="#list-example" data-bs-smooth-scroll="true" class="scrollspy-example float-center" tabindex="0">
+  <div class="card">
+  <div class="card-body" id="list-item-1">
+   <b> Header </b><br><br>
+<form action="" method="post">
+<div class="mb-3">
+  <label for="nama-website" class="form-label">Nama Website</label>
+  <input type="text" class="form-control" id="nama-website" placeholder="Masukkan Nama Website disini">
+</div>
+<div class="mb-3">
+<label for="placeholder-pencarian" class="form-label">Placeholder Pencarian</label>
+  <input type="text" class="form-control" id="placeholder-pencarian" placeholder="Masukkan Placeholder Pencarian disini">
+</div>
+
+<button type="submit" class="btn btn-success"> Simpan </button>
+</form>
+  </div>
+</div>
+</div>
+</body>
+</html>
